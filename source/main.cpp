152fc456d3c756affdd096a2f002834ab41218b2
mstrVLT <mstrvlt@gmail.com>
2020-06-23 13:21:53 +0300

    init

---
+++ source/main.cpp
@@ -0,0 +1,224 @@
+﻿// dear imgui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+
+#include "imgui.h"
+#include "imgui_impl_glfw_switch.h"
+#include "imgui_impl_opengl3.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+#define GLM_FORCE_PURE
+#define GLM_ENABLE_EXPERIMENTAL
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/rotate_vector.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+
+#ifdef __SWITCH__
+#include <switch.h>
+#endif
+
+#include <chrono>
+#include <set>
+#include <thread>
+
+// Constants used for scaling as well as
+// creating a window of the right size on PC
+constexpr uint32_t WINDOW_WIDTH  = 1280;
+constexpr uint32_t WINDOW_HEIGHT = 720;
+
+#define DEFAULT_FPS 60
+
+static void errorCallback(int errorCode, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", errorCode, description);
+}
+
+int main(int, char**)
+{
+    socketInitializeDefault();
+
+    nxlinkStdio();
+
+    // Init rng
+    std::srand(std::time(nullptr));
+    
+    // Init glfw
+    glfwSetErrorCallback(errorCallback);
+
+    // glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE);
+
+    if (!glfwInit())
+    {
+        return false;
+    }
+    const char* glsl_version = "#version 430 core";
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "test", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return false;
+    }
+
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GLFW_TRUE);
+    glfwMakeContextCurrent(window);
+
+    // Load OpenGL routines using glad
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableSetMousePos;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Load Fonts
+    io.Fonts->AddFontDefault();
+    {
+        plInitialize(PlServiceType_System);
+        static PlFontData stdFontData, extFontData;
+    
+        PlFontData fonts_std;
+        PlFontData fonts_ext;
+        
+        plGetSharedFontByType(&fonts_std, PlSharedFontType_Standard);
+        plGetSharedFontByType(&fonts_ext, PlSharedFontType_NintendoExt);
+
+        ImFontConfig config;
+        config.FontDataOwnedByAtlas = false;
+
+        strcpy(config.Name, "Nintendo Standard");
+        io.Fonts->AddFontFromMemoryTTF (fonts_std.address, fonts_std.size, 24.0f, &config, io.Fonts->GetGlyphRangesCyrillic());
+
+        strcpy(config.Name, "Nintendo Ext");
+        static const ImWchar ranges[] =
+            {
+                0xE000, 0xE06B,
+                0xE070, 0xE07E,
+                0xE080, 0xE099,
+                0xE0A0, 0xE0BA,
+                0xE0C0, 0xE0D6,
+                0xE0E0, 0xE0F5,
+                0xE100, 0xE105,
+                0xE110, 0xE116,
+                0xE121, 0xE12C,
+                0xE130, 0xE13C,
+                0xE140, 0xE14D,
+                0xE150, 0xE153,
+                0,
+            };
+
+        io.Fonts->AddFontFromMemoryTTF (fonts_ext.address, fonts_ext.size, 24.0f, &config, ranges);
+        io.Fonts->Build ();
+
+        plExit();
+    }
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    socketExit();
+
+    return 0;
+}
